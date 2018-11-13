d535941b2b614d74f4731d491bf16505f83f5b65
drace <daren.race@gmail.com>
2018-11-12 16:56:01 -0800

    added more types of data

--- og.py
+++ og.py
@@ -7,27 +7,32 @@ import json
 import re
 from bs4 import BeautifulSoup
 
+
 def grab_meta_tags(soup, patterns):
     result = {}
-    for k,v in patterns:
+    for k, v in patterns:
         for meta in soup.findAll(attrs={k: re.compile(v)}):
             if meta and meta.has_attr('content'):
                 result[meta[k].replace(':', '_')] = meta['content']
     return result
 
-def get_og_tags(url):
-    page = requests.get(url, headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36',
-            'Referer': 'https://googlebot.com',
-            'Accept-Charset': 'utf-8',
-            "Accept-Encoding": "identity, deflate, compress, gzip",
-            'Connection': 'close'
-        })
 
-    return grab_meta_tags(BeautifulSoup(page.text, 'lxml'), [('property', 'og:*'), ('name', 'twitter:*')])
-
+def get_og_tags(url, headers):
+    page = requests.get(url, headers=headers)
+    return grab_meta_tags(BeautifulSoup(page.text, 'lxml'), [
+        ('property', 'og:*'),
+        ('name', 'og:*'),
+        ('name', 'sailthru[.]*'),
+        ('name', 'twitter:*')])
 
 
 if __name__ == "__main__":
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36',
+        'Referer': 'https://googlebot.com',
+        'Accept-Charset': 'utf-8',
+        "Accept-Encoding": "identity, deflate, compress, gzip",
+        'Connection': 'close'
+    }
     for url in sys.argv[1:]:
-        print json.dumps(get_og_tags(url), indent=3)
+        print json.dumps(get_og_tags(url, headers), indent=2)
